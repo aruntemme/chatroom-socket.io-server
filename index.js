f3f60c2de7be33242cbb07d674720b0a841dce03
aruntemme <arungauthamk@gmail.com>
2021-08-26 21:05:21 +0530

    client-url-update

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const app = require('express');
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors())
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
   cors: {
-      origin: "http://chatroom-socket.surge.sh/",
+    origin: "http://chatroom-socket.surge.sh/",
+    methods: ["GET", "POST"]
   }
 });
+
 
 io.on('connection', socket => {
   socket.on('message', ({name, message}) => {
@@ -12,6 +19,6 @@ io.on('connection', socket => {
   })
 })
 
-http.listen(4000, () => {
-  console.log('listening on 8000')
+server.listen(4000, () => {
+  console.log('listening on 4000')
 })
